adbfb5d0bc2847feda7584268d02c4dfd1ab662c
yannafaith <yannafaith97@gmail.com>
2019-02-19 16:57:20 -0700

    adds bear endpoints / finishes stretch

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+/* =============== Zoos endpoints ================ */
+
 
 //  ============== GET Endpoints 
 
@@ -88,8 +90,81 @@ server.put('/api/zoos/:id', async (req, res) => {
   } catch (error) {}
 });
 
+/* =============== Bears endpoints ================ */
+
+//  ============== GET Endpoints 
+
+server.get('/api/bears/', async (req, res) => {
+  try {
+    const bears = await db('bears')
+    res.status(200).json(bears);
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
+
+server.get('/api/bears/:id', async (req, res) => {
+  try {
+    const bear = await db('bears')
+    .where({ id: req.params.id })
+    .first()
+    res.status(200).json(bear);
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
+
+//  ============== POST Endpoint
+
+server.post('/api/bears', (req, res) => {
+  const bear = req.body;
+  db.insert(bear)
+    .into("bears")
+    .then( bear => {
+      res.status(201).json(bear)
+    })
+    .catch(() => {
+      res.status(500).json({error: "we encountered an error"})
+    })
+});
+
+//  ============== DELETE Endpoint
+
+server.delete('/api/bears/:id', (req, res) => {
+  db('bears')
+  .where({id: req.params.id})
+  .del()
+  .then(() => {
+    res.status(200).json({message: "bear deleted"})
+  })
+  .catch(() => {
+    res.status(500).json({error: "error"})
+  })
+});
+
+//  ============== PUT Endpoint
+
+server.put('/api/bears/:id', async (req, res) => {
+  try {
+    const count = await db('bears')
+      .where({ id: req.params.id })
+      .update(req.body);
+
+    if (count > 0) {
+      const bear = await db('bears')
+        .where({ id: req.params.id })
+        .first();
+
+      res.status(200).json(bear);
+    } else {
+      res.status(404).json({ message: 'Records not found' });
+    }
+  } catch (error) {}
+});
+
+
 
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+  console.log(`\n **** Web API Listening on http://localhost:${port} **** \n`);
 });
